1d17890ac5fa66ec66b234985dde986a757306ed
Daniel Giorgetti <danielgiorgetti@gmail.com>
2024-09-08 18:39:37 -0400

    initial commit

---
+++ install-browser.ts
@@ -0,0 +1,18 @@
+import {
+    Browser,
+    install,
+    resolveBuildId,
+    detectBrowserPlatform,
+    BrowserPlatform,
+} from '@puppeteer/browsers';
+
+
+async function main() {
+    const cacheDir = __dirname + "/.cache";
+    const browser = Browser.CHROME;
+    const platform: BrowserPlatform = detectBrowserPlatform() || BrowserPlatform.LINUX;
+    const buildId = await resolveBuildId(browser, platform, 'stable');
+    await install({ browser, buildId, cacheDir });
+}
+
+main();
